9c2dc73347852662df747791052c70c2e2463af9
Buns <bunsthedev@gmail.com>
2022-04-12 00:22:35 -0500

    update: userInfo

--- api/users/index.js
+++ api/users/index.js
@@ -1,16 +1,49 @@
 'use strict';
 const {web3Factory} = require("../../utils/web3");
-const { FTM_CHAIN_ID, LUM, SOR } = require("../../constants");
+const { FTM_CHAIN_ID, LUM, SOR, AURA, MULTICALL_ADDRESS, SOUL_DAO, AUTOSTAKE_ADDRESS } = require("../../constants");
 
 const web3 = web3Factory( FTM_CHAIN_ID );
 const ERC20ContractABI = require('../../abis/ERC20ContractABI.json');
+const MulticallContractABI = require('../../abis/MulticallContractABI.json');
 const PriceFetcherABI = require('../../abis/PriceFetcherABI.json');
 const fetcherAddress = '0xba5da8aC172a9f014D42837EE1B678C4Ca96fB0E';
+const AuraContract = new web3.eth.Contract(ERC20ContractABI, AURA);
+const AutoStakeContract = new web3.eth.Contract(ERC20ContractABI, AUTOSTAKE_ADDRESS);
+const MulticallContract = new web3.eth.Contract(MulticallContractABI, MULTICALL_ADDRESS);
+
 const BN = require('bn.js');
 
+async function getUserInfo(ctx) {
+    const userAddress = web3.utils.toChecksumAddress(ctx.params.id);
+    const nativeBalance = await MulticallContract.methods.getEthBalance(userAddress).call() / 1e18;
+    const votingPower =  await AuraContract.methods.balanceOf(userAddress).call() / 1e18;
+    const ineligiblePower = await AuraContract.methods.balanceOf(SOUL_DAO).call() / 1e18;
+    const totalVotingPower 
+        = await AuraContract.methods.totalSupply().call() / 1e18;
+    const eliblePower = totalVotingPower - ineligiblePower;
+    const stakedBalance =  await AutoStakeContract.methods.balanceOf(userAddress).call() / 1e18;
+    const protocolOwnership =  votingPower / eliblePower * 100
+    if (!("id" in ctx.params))
+        return {"name": "Users"};
+    else {
+        return {
+            "address": userAddress,
+            "votingPower": votingPower,
+            "ineligiblePower": ineligiblePower,
+            "eliblePower": eliblePower,
+            "nativeBalance": nativeBalance,
+            "stakedBalance": stakedBalance,
+            "protocolOwnership": protocolOwnership,
+            "api": `https://api.soulswap.finance/info/users/${userAddress}`,
+            "ftmscan": `https://ftmscan.com/address/${userAddress}`,
+        }
+    }
+}
+
 async function getTokenInfo(ctx) {
-    const tokenAddress = web3.utils.toChecksumAddress(ctx.params.id);
-    const userAddress = web3.utils.toChecksumAddress(ctx.params.userAddress);
+    const userAddress = web3.utils.toChecksumAddress(ctx.params.id);
+    const address = web3.utils.toChecksumAddress(ctx.params.tokenAddress);
+
     const TokenContract = new web3.eth.Contract(ERC20ContractABI, tokenAddress);
     const PriceFetcherContract = new web3.eth.Contract(PriceFetcherABI, fetcherAddress);
 
@@ -35,7 +68,7 @@ async function getTokenInfo(ctx) {
         return {"name": "Users"};
     else {
         return {
-            "address": tokenAddress,
+            "address": address,
             "name": tokenName,
             "symbol": tokenSymbol,
             "balance": tokenBalance,
@@ -44,15 +77,19 @@ async function getTokenInfo(ctx) {
             "decimals": tokenDecimals,
             "supply": totalSupply,
             "mcap": marketCap,
-            "api": `https://api.soulswap.finance/info/tokens/${tokenAddress}`,
-            "ftmscan": `https://ftmscan.com/address/${tokenAddress}#code`,
+            "api": `https://api.soulswap.finance/info/users/${userAddress}/${tokenAddress}`,
+            "ftmscan": `https://ftmscan.com/address/${address}#code`,
             "image": `https://raw.githubusercontent.com/soulswapfinance/assets/master/blockchains/fantom/assets/${ctx.params.id}/logo.png`
         }
     }
+}
+
+async function userInfo(ctx) {
+    ctx.body = (await getUserInfo(ctx))
 }
 
 async function tokenInfo(ctx) {
     ctx.body = (await getTokenInfo(ctx))
 }
 
-module.exports = { tokenInfo };
+module.exports = { userInfo, tokenInfo };
