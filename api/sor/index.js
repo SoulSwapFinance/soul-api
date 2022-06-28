f2863e08ac94c605962f8213a51380fbf15d0d64
Buns <bunsthedev@gmail.com>
2022-06-27 22:03:37 -0500

    update: SOR

--- api/sor/index.js
+++ api/sor/index.js
@@ -1,6 +1,6 @@
 'use strict';
 const {web3Factory} = require("../../utils/web3");
-const { DAI, LUX, FTM_CHAIN_ID, LUXOR_TREASURY_ADDRESS } = require("../../constants");
+const { DAI, LUX, WLUM, FTM_CHAIN_ID, LUXOR_TREASURY_ADDRESS } = require("../../constants");
 
 const web3 = web3Factory( FTM_CHAIN_ID );
 const ERC20ContractABI = require('../../abis/ERC20ContractABI.json');
@@ -10,30 +10,35 @@ const BN = require('bn.js');
 
 async function getSorInfo() {
     const luxorTreasuryAddress = LUXOR_TREASURY_ADDRESS;
-    const luxorAddress = LUX;
     const sorAddress = '0xEFFd4874AcA3Acd19a24dF3281b5cdAdD823801A';
-    const masterAddress = '0xfF1157CaCB174c012f68CDb0B7700597aae3D5A8';
+    // const masterAddress = '0xfF1157CaCB174c012f68CDb0B7700597aae3D5A8';
+    const minterAddress = '0x104b191008e56a0B79f25a828Ee18873AdD36C6c'
     const SorContract = new web3.eth.Contract(ERC20ContractABI, sorAddress);
     const DaiContract = new web3.eth.Contract(ERC20ContractABI, DAI);
     const LuxorContract = new web3.eth.Contract(ERC20ContractABI, LUX);
+    const WLumensContract = new web3.eth.Contract(ERC20ContractABI, WLUM);
     const fetcherAddress = '0xba5da8aC172a9f014D42837EE1B678C4Ca96fB0E';
     const PriceFetcherContract = new web3.eth.Contract(PriceFetcherABI, fetcherAddress);
 
     // METHOD CALLS //
-    const rawPrice = await PriceFetcherContract.methods.currentTokenUsdcPrice(luxorAddress).call();
-    const luxorPrice = rawPrice / 1e18
+    const rawLuxorPrice = await PriceFetcherContract.methods.currentTokenUsdcPrice(LUX).call();
+    const rawWrappedLumensPrice = await PriceFetcherContract.methods.currentTokenUsdcPrice(WLUM).call();
+    const luxorPrice = rawLuxorPrice / 1e18
+    const wlumPrice = rawWrappedLumensPrice / 1e18
 
     const totalSupply = await SorContract.methods.totalSupply().call() / 1e18;
     const luxorTreasuryBalance = await SorContract.methods.balanceOf(luxorTreasuryAddress).call() / 1e18;
-    const sorMasterCollateral = await SorContract.methods.balanceOf(masterAddress).call() / 1e18;
-    const daiMasterCollateral = await DaiContract.methods.balanceOf(masterAddress).call() / 1e18;
-    const luxorMasterCollateral = await LuxorContract.methods.balanceOf(masterAddress).call();
+    const sorMasterCollateral = await SorContract.methods.balanceOf(minterAddress).call() / 1e18;
+    const daiMasterCollateral = await DaiContract.methods.balanceOf(minterAddress).call() / 1e18;
+    const luxorMasterCollateral = await LuxorContract.methods.balanceOf(minterAddress).call() / 1e9;
+    const wlumMasterCollateral = await WLumensContract.methods.balanceOf(minterAddress).call() / 1e9;
+
     const stableCollateralValue = sorMasterCollateral + daiMasterCollateral
-    const luxorCollateralValue = luxorMasterCollateral * luxorPrice / 1e9
+    const luxorCollateralValue = luxorMasterCollateral * luxorPrice
+    const wlumCollateralValue = wlumMasterCollateral * wlumPrice
     const collateralValue = luxorCollateralValue + stableCollateralValue
     const collateralization = collateralValue / totalSupply
-    // TODO: burn SOR
-
+s
         return {
             "address": sorAddress,
             "supply": totalSupply,
@@ -43,7 +48,7 @@ async function getSorInfo() {
             "stableCollateral": stableCollateralValue,
             "luxorCollateral": luxorMasterCollateral,
             "luxorCollateralValue": luxorCollateralValue,
-            "luxorCollateralValue": luxorCollateralValue,
+            "wlumCollateralValue": wlumCollateralValue,
             "collateralValue": collateralValue,
             "collateralization": collateralization,
             "api": "https://api.soulswap.finance/sor",
